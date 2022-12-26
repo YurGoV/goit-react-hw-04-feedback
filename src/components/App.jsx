2368827370091af7122f87b7eeb7436ce7c0e347
YurGoV <yurgov@gmail.com>
2022-12-26 17:20:52 +0200

    App-to-hoohs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,59 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import {useState} from "react";
 import { Stat } from './App.styled';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Section } from './Section/Section';
 import { NoFeedbackNotification } from './Notification/Notification';
 
-export class App extends Component {
+
+const keys = ['good', 'neutral', 'bad']
+
+export const App = () => {
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+
+  const changeVoteState = (data) => {
+    data === 'good' ? setGood(good + 1) :
+      data === 'neutral' ? setNeutral(neutral + 1) :
+        data === 'bad' ? setBad(bad + 1) : console.log('error');
+  }
+    // console.log('data: ', data);
+
+  const isFeedback = () => good > 0 || neutral > 0 || bad > 0;
+
+
+  return (
+
+    <Section title='Please Leave Feedback'>
+
+      <FeedbackOptions
+        options={keys}
+        onVote={changeVoteState}>
+      </FeedbackOptions>
+
+      <Stat isData={isFeedback()}>Statistics</Stat>
+      {isFeedback() ? (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}>
+        </Statistics>
+      ) : (
+        <NoFeedbackNotification message='There is no feedback'></NoFeedbackNotification>
+      )}
+
+    </Section>
+  );
+}
+
+
+/*
+
+export class OldApp extends Component {
 
   constructor() {
     super();
@@ -55,3 +103,4 @@ export class App extends Component {
   }
 }
 
+*/
